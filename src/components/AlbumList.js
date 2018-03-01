90cadfbd65eea6ceb216f89f6856813517cfa892
Neto6391 <neto6391@gmail.com>
2018-03-01 12:49:09 -0300

    Update AlbumList.js

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -12,13 +12,7 @@ class AlbumList extends Component {
     }
     
     //Generate the list of albums details
-    renderAlbums() {
-        /* # É um array help, sendo que ele funciona como uma arrow function em que percorrerá 
-           o array e retornará os elementos;
-           # A propriedade 'key' é o que react utilizará para descobrir qual item da lista é 
-           atualizado em tempo real, em que o único requerimento ele seja único entre outros
-           elementos no array e que ele tenha o mesmo valor da prop que redenrizamos a lista;
-        */        
+    renderAlbums() {       
         return this.state.albums.map(album => 
             // <Text key={album.title}>{album.title}</Text> 
             <AlbumDetail key={album.title} albumData={album} />  
